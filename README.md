54fa7a593d4a86ac16371d50625430b15c6ebf51
Ali Fathieh <ali.fathieh@gmail.com>
2016-02-03 22:15:15 +1100

    init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # meetup-ticket-selling
 Adding online ticket selling to events on meetup.com website
+
+# HOW to run
+- terminal in new tab
+
+    > npm i
+
+    > webpack -wd
+
+- terminal in new tab
+
+    > export MONGO_URL=mongodb://<dbuser>:<dbpassword>@ds055505.mongolab.com:55505/ticket_event
+    
+    > nodemon
+    
+- navigate to localhost:3000
---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import schema from './data/graphql/schema';
+import GraphQLHTTP from 'express-graphql';
+import {MongoClient} from 'mongodb';
+
+let app = express();
+app.use(express.static('public'));
+
+app.use('/graphql', GraphQLHTTP({
+	schema: schema,
+	graphiql: true
+}));
+
+let db;
+console.log('process.env.MONGO_URL',process.env.MONGO_URL);
+MongoClient.connect(process.env.MONGO_URL, (err, database) => {
+  if (err) 
+  {
+  	console.log(err);
+  	throw err;
+  }
+
+  db = database;
+  let listener = app.listen(3000, () => console.log('Listening on port 3000'));
+});
+
+app.get("/data/links", (req, res) => {
+  db.collection("links").find({}).toArray((err, links) => {
+    if (err) throw err;
+
+    res.json(links);
+  });
+});
